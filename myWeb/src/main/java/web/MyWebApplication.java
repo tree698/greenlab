c9238437e1a1e06268a94373973c1f4813f83e7d
KHKIM <hobbitbears@gmail.com>
2020-10-06 01:53:24 +0900

    tomcat 설절 수정

--- myWeb/src/main/java/web/MyWebApplication.java
+++ myWeb/src/main/java/web/MyWebApplication.java
@@ -3,12 +3,19 @@ package web;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication(exclude = { SecurityAutoConfiguration.class })
-public class MyWebApplication {
+public class MyWebApplication extends SpringBootServletInitializer {
+	
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		
+		return super.configure(builder.sources(MyWebApplication.class));
+	}
 
 	public static void main(String[] args) {
-		
 		SpringApplication.run(MyWebApplication.class, args);
 	}
 
