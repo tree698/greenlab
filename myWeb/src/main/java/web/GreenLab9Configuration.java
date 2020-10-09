3503264e8be4a988a2bae9a338defa5e3d789b96
KHKIM <hobbitbears@gmail.com>
2020-10-10 00:43:01 +0900

    파일업로드 d&d 수정

--- myWeb/src/main/java/web/GreenLab9Configuration.java
+++ myWeb/src/main/java/web/GreenLab9Configuration.java
@@ -2,6 +2,7 @@ package web;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -16,5 +17,12 @@ public class GreenLab9Configuration implements WebMvcConfigurer {
 	@Override
 	public void addInterceptors(InterceptorRegistry registry) {
 		registry.addInterceptor(handlerInterceptorEx).addPathPatterns("/*");
+	}
+	@Override
+	public void addCorsMappings(CorsRegistry registry) {
+		registry.addMapping("/**")
+				.allowedOrigins("*")
+				.allowedMethods("GET", "POST")
+				.maxAge(3000);
 	}
 }
--- myWeb/src/main/java/web/controller/PhotoRestController.java
+++ myWeb/src/main/java/web/controller/PhotoRestController.java
@@ -14,6 +14,7 @@ import org.springframework.http.MediaType;
 import org.springframework.util.DigestUtils;
 import org.springframework.util.FileCopyUtils;
 import org.springframework.util.StringUtils;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
