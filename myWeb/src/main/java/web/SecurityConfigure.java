ab53aa087506fb7cdadd304402ba1217428ab088
KHKIM <hobbitbears@gmail.com>
2020-10-12 00:13:40 +0900

    로그인기능 수정

--- myWeb/src/main/java/web/SecurityConfigure.java
+++ myWeb/src/main/java/web/SecurityConfigure.java
@@ -1,20 +1,19 @@
 package web;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
-import web.service.UserService;
-
 @EnableWebSecurity
-public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+public class SecurityConfigure extends WebSecurityConfigurerAdapter {
 	
 	@Autowired
 	UserService userService;
@@ -37,6 +36,7 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 			.antMatchers("/**").permitAll();
 		
 		http.formLogin()
+			.loginPage("/login")
 			.loginProcessingUrl("/login/process")
 			.defaultSuccessUrl("/");
 
@@ -48,6 +48,14 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 	
 	@Override
 	public void configure(AuthenticationManagerBuilder auth) throws Exception {
+		// auth.userDetailsService(getApplicationContext().getBean(UserDetailsService.class)).passwordEncoder(passwordEncoder());
 		auth.userDetailsService(userService).passwordEncoder(passwordEncoder());
 	}
+	
+	/*
+	@Bean
+	public AuthenticationManager authenticationManagerBean() throws Exception {
+		return super.authenticationManagerBean();
+	}
+	*/
 }
