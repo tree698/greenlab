8be2ee2f8b26913b3b8c888967f7c1cda63bcd98
KHKIM <hobbitbears@gmail.com>
2020-10-19 23:51:29 +0900

    이메일 기능 뼈대 생성

---
+++ myWeb/src/main/java/web/service/EmailService.java
@@ -0,0 +1,28 @@
+package web.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Service;
+
+import lombok.extern.slf4j.Slf4j;
+
+/**
+ * Email Serivce
+ */
+@Slf4j
+@Service
+public class EmailService {
+
+	@Autowired
+	private JavaMailSender emailSender;
+
+	public void sendSimpleMessage(String to, String subject, String text) {
+		SimpleMailMessage message = new SimpleMailMessage();
+		message.setFrom("hobbitbears@greenlab9.com");
+		message.setTo(to);
+		message.setSubject(subject);
+		message.setText(text);
+		emailSender.send(message);
+	}
+}
